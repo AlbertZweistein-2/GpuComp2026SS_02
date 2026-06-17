57b723a1dedbde582e21fd24a8eaf5ef99cc8160
Philipp Sepin <151570662+p0017@users.noreply.github.com>
2026-06-17 17:54:06 +0200

    Fixed stb_image import

--- src/parallel/visualization.cpp
+++ src/parallel/visualization.cpp
@@ -1,15 +1,9 @@
 #include "parallel/visualization.hpp"
 
+#define STB_TRUETYPE_IMPLEMENTATION
 #include "stb_truetype.h"
-
-#if defined(__clang__)
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wdeprecated-declarations"
-#endif
+#define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
-#if defined(__clang__)
-#pragma clang diagnostic pop
-#endif
 
 #include <algorithm>
 #include <fstream>
